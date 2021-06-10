cb285bb147c6ece8f6f484cf30c32eb775d7ce75
Ecv Digital <ecvmaster20.21@gmail.com>
2021-06-10 10:51:47 +0200

    update

--- BlindTest/app.js
+++ BlindTest/app.js
@@ -1,23 +1,26 @@
 const fs = require("fs");
 var http = require("http");
+const { data } = require("jquery");
 
-var client_id = '66ba962682654c159063a75a767d5255'; // Your client id
-var client_secret = 'aa80334eb8864f1a8698c47e7fe418d2'; // Your secret
-var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
+var SpotifyWebApi = require('spotify-web-api-node');
 
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-var xhr = new XMLHttpRequest();
-
-var SpotifyWebApi = require('spotify-web-api-js');
-
-var spotifyApi = new SpotifyWebApi();
-
-spotifyApi.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE', function(err, data) {
-    if (err) console.error(err);
-    else console.log('Artist albums', data);
+// credentials are optional
+var spotifyApi = new SpotifyWebApi({
+    clientId: '66ba962682654c159063a75a767d5255', // Your client id
+    clientSecret: 'aa80334eb8864f1a8698c47e7fe418d2', // Your secret
+    redirectUri: 'http://localhost:8888/callback' // Your redirect uri
 });
 
+spotifyApi.setAccessToken('BQDiJcV1chnwrCEkQvGujyql0daCWtzPwOAW8gxHJKkhd-m6HLrj2cOry44PTNltKABXuDvtGYTt32qHHNA7qlZXrKKvZQzznQ-BF1RI438A7bS9U1T7nTxGjaakn7-HCQAvC1xqItCJ5n26iGtqn6Fjv95-tEGYH3wdM9UUTt9igiIWLCr5HObrrG05I-rT_CO5');
 let server = http.createServer()
+
+spotifyApi.getArtist('3Y7RZ31TRPVadSFVy1o8os')
+    .then(function(data) {
+        console.log('Artist information', data.body.name);
+    }, function(err) {
+        console.error(err);
+    });
+
 
 server.on('request', (request, response) => {
     fs.readFile('index.html', (err, data) => {
@@ -29,4 +32,4 @@ server.on('request', (request, response) => {
     })
 })
 
-server.listen(8080)
+server.listen(8888)
